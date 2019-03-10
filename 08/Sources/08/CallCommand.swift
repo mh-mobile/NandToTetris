b2c77e986d423c5f5533bd30fa37fd5ab134759f
mbyk <marubayashi@gmail.com>
2019-03-11 08:33:11 +0900

    CallCommandの実装

--- 08/Sources/08/CallCommand.swift
+++ 08/Sources/08/CallCommand.swift
@@ -15,6 +15,68 @@ extension CallCommand: Command {
     func convert() -> String {
         let asm = """
                   // call
+                  @\(functionName)#return-address
+                  D=A
+                  @SP
+                  A=M
+                  M=D
+                  @SP
+                  M=M+1
+
+                  @LCL
+                  D=M
+                  @SP
+                  A=M
+                  M=D
+                  @SP
+                  M=M+1
+
+                  @ARG
+                  D=M
+                  @SP
+                  A=M
+                  M=D
+                  @SP
+                  M=M+1
+
+                  @THIS
+                  D=M
+                  @SP
+                  A=M
+                  M=D
+                  @SP
+                  M=M+1
+
+                  @THAT
+                  D=M
+                  @SP
+                  A=M
+                  M=D
+                  @SP
+                  M=M+1
+
+                  @SP
+                  D=M
+                  @LCL
+                  M=D
+
+                  @5
+                  D=A
+                  @LCL
+                  D=M-D
+                  @R13
+                  M=D
+                  @\(numArgs)
+                  D=A
+                  @R13
+                  D=M-D
+                  @ARG
+                  M=D
+
+                  @\(functionName)
+                  0;JPM
+                  
+                  (\(functionName)#return-address)
                   """
         return asm
     }
